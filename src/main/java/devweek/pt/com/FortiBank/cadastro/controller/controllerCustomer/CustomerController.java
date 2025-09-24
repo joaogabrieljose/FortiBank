78f9a32b608369426a4556f5324a702aa2d92290
User <user@MacBook-Pro-de-User.local>
2025-09-24 17:59:57 +0100

    customerController

---
+++ src/main/java/devweek/pt/com/FortiBank/cadastro/controller/controllerCustomer/CustomerController.java
@@ -0,0 +1,28 @@
+package devweek.pt.com.FortiBank.cadastro.controller.controllerCustomer;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import devweek.pt.com.FortiBank.cadastro.model.entityCustomer.dto.CustomerDTO;
+import devweek.pt.com.FortiBank.cadastro.useCase.useCaseCustomer.CreateUseCaseCustomer;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    private CreateUseCaseCustomer useCase;
+
+    public CustomerController(CreateUseCaseCustomer useCase){
+        this.useCase = useCase;
+    }
+
+    @PostMapping("/")
+    public String create (@RequestBody CustomerDTO dto){
+        this.useCase.execute(dto);
+        return "Cliente registado...";
+    }
+    
+}
