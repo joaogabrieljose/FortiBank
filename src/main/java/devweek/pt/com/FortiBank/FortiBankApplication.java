634a625d6d9e00ae2aa9bdcb8da46005a0dcb91a
User <user@MacBook-Pro-de-User.local>
2025-09-24 16:07:20 +0100

    init

---
+++ src/main/java/devweek/pt/com/FortiBank/FortiBankApplication.java
@@ -0,0 +1,13 @@
+package devweek.pt.com.FortiBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortiBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devweek/pt/com/FortiBank/FortiBankApplicationTests.java
@@ -0,0 +1,13 @@
+package devweek.pt.com.FortiBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortiBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
